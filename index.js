96b077aec23c0f8d9edff204a1d644792d5f8c1a
Mark Wanjau <theafrikanson@gmail.com>
2022-11-27 11:48:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000 / 100;
+    }
+    divide();
+    
+    const square = x =>  x * x;
+    square(5);
+    
+    const add = (a,b) => a  + b;
+    add(a,b);
